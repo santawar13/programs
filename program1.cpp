f6a06105365116d4731ff520ee6848ad1f387b25
santawar13 <74447838+santawar13@users.noreply.github.com>
2021-11-23 13:57:20 +0400

    Add files via upload

---
+++ program1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool palidrom(string);
+//Task
+int main() {
+	string x;
+	cin>>x;
+	palidrom(x);
+
+    return 0;
+}
+bool palidrom(string text){
+	string y = text;
+    reverse(text.begin(),text.end());
+    if(y==text)
+    cout<<"yes this is palidrom";
+    else
+    cout<<"this isn't palidrom";
+
+}
+
